c4bd1a1b4058327e0928b8d655f8c874a50b1f1a
Yogesh Kanhegawankar <kanhegawankaryogesh@gmail.com>
2024-09-07 20:03:28 +0530

    first commit

---
+++ src/pages/Doctors.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {useParams} from 'react-router-dom'
+
+
+const Doctors = () => {
+
+  const { speciality } = useParams()
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Doctors
